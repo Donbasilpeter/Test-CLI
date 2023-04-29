8a3147a21a81e43b59381b1686d5fbe97e7b40c3
Don Basil Peter <donbasilpeter@gmail.com>
2023-04-29 19:18:44 +0530

    title added

--- utils/init.js
+++ utils/init.js
@@ -5,7 +5,7 @@ const unhandled = require('cli-handle-unhandled');
 module.exports = ({ clear = true }) => {
 	unhandled();
 	welcome({
-		title: ``,
+		title: `Test-CLI`,
 		tagLine: `by Don Basil Peter`,
 		description: pkg.description,
 		version: pkg.version,
